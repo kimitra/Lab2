9a331ee5e72718d2b6390a53f96e6a3fc956f1b4
kimitra <vmitraki@purdue.edu>
2024-01-17 12:02:28 -0500

    Experimented with code and created equation

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -1,20 +1,23 @@
-// Lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	float A;
+	float B;
+	float X;
+	cout << "Hello, my name is Kiki and I'm going to solve the equation:" << endl;
+	cout << "Ax + B = 0" << endl;
+	cout << "for x" << endl;
+	cout << endl;
+	cout << "Please enter a value for A: ";
+	cin >> A;
+	cout << "Please enter a value for B: ";
+	cin >> B;
+	cout << endl;
+	cout << "Solving "<< A <<"+"<< B <<"=0 for x...";
+	cout << endl;
+
+	X = (0 - B) / A;
+	cout << "The answeri is:" << endl;
+	cout << "x=" << X << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
